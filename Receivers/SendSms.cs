afc69f298218f70947c0d2fd083bdbf5da1894e8
玮仔Wayne <owner@wayneshao.com>
2022-05-16 23:01:45 +0800

    初始化，测试有效

---
+++ Receivers/SendSms.cs
@@ -0,0 +1,18 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Telephony;
+
+namespace XamarinSMS.Receivers
+{
+    public class SendSms
+    {
+        
+        public void Send(string address, string message)
+        {
+            var pendingIntent = PendingIntent.GetActivity(Android.App.Application.Context, 0, new Intent(Android.App.Application.Context, typeof(MainActivity)).AddFlags(ActivityFlags.ClearTop | ActivityFlags.NewTask), PendingIntentFlags.NoCreate);
+
+            SmsManager smsM = SmsManager.Default;
+            smsM.SendTextMessage(address, null, message, pendingIntent, null);
+        }
+    }
+}
