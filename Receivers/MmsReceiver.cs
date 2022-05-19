45616d3b86049e64cbbcf3eaadabde71d40b14ee
玮仔Wayne <owner@wayneshao.com>
2022-05-19 16:38:13 +0800

    可以设置为默认程序

---
+++ Receivers/MmsReceiver.cs
@@ -0,0 +1,15 @@
+﻿using Android.App;
+using Android.Content;
+
+namespace XamarinSMS.Receivers
+{
+    [BroadcastReceiver(Enabled = true, Exported = true, Permission = "android.permission.BROADCAST_WAP_PUSH")]
+    [IntentFilter(new[] { "android.provider.Telephony.WAP_PUSH_DELIVER" })]
+    public class MmsReceiver: BroadcastReceiver
+    {
+        public override void OnReceive(Context? context, Intent? intent)
+        {
+            
+        }
+    }
+}
